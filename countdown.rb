3cea3e08aa0c446bae8097fbf1f3de7e7d7e8fc8
Chindalath Traymany <chindalatht@gmail.com>
2020-07-07 07:37:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  int <= 60
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int-=
+  end
+  if int = 0
+    "HAPPY NEW YEAR!"
+  end
 end
